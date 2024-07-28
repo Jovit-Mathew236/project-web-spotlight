c539a98f7e78a087758cd3c2f118063462d0a759
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-07-28 14:39:16 +0530

    feat: added theme provider and make default dark theme

--- src/components/ui/modules/spotlight.tsx
+++ src/components/ui/modules/spotlight.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Bell, Inbox, Search } from "lucide-react";
 
 import {
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: "es2015",
+    rollupOptions: {
+      output: {
+        format: "iife",
+        entryFileNames: "bundle.js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+        manualChunks: undefined,
+      },
+    },
+    cssCodeSplit: false,
+    assetsInlineLimit: 0,
+    minify: "terser",
+    terserOptions: {
+      compress: { drop_console: true, drop_debugger: true },
     },
   },
 });
